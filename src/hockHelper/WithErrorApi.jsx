aa1983b878c9701442df4311fb58a8bfedbff8f5
Oknokus <oknokus@gmail.com>
2023-01-27 04:08:14 +0300

    home-work: tasks 7(part1), creating a hockHelper(WithErrorApi & component ErrorMessage)

--- src/hockHelper/WithErrorApi.jsx
+++ src/hockHelper/WithErrorApi.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
+import ErrorMessage from "../components/ErrorMessage"
+
 const withErrorApi = View => {
     return props => {
         const [errorApi, setErrorApi] = useState(false);
         return (
             <>
             {errorApi ?
-            <h1>ERROR!!!</h1> : (
+            <ErrorMessage /> : (
             <View
             setErrorApi={setErrorApi}
             {...props}
