85209c09cb71ba58287a1bd94038335fb55c0177
Oknokus <oknokus@gmail.com>
2023-03-09 00:57:25 +0300

    production2

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -36,9 +36,8 @@ export const VISIAL_GUIDE_EXTENSION = ".jpg";
 
 
 // PersonPage id
-export const PERSON_PAGE_PEOPLE_PATH_ID = "https://oknokus.github.io/react-starwars_work-project/people/";
-export const PERSON_PAGE_SPECIES_PATH_ID = "https://oknokus.github.io/react-starwars_work-project/species/";
-export const PERSON_PAGE_STARSHPS_PATH_ID = "https://oknokus.github.io/react-starwars_work-project/starships/";
-export const PERSON_PAGE_VEHICLES_PATH_ID = "https://oknokus.github.io/react-starwars_work-project/vehicles/";
-
+export const PERSON_PAGE_PEOPLE_PATH_ID = "http://192.168.100.14:3000/people/";
+export const PERSON_PAGE_SPECIES_PATH_ID = "http://192.168.100.14:3000/species/";
+export const PERSON_PAGE_STARSHPS_PATH_ID = "http://192.168.100.14:3000/starships/";
+export const PERSON_PAGE_VEHICLES_PATH_ID = "http://192.168.100.14:3000/vehicles/";
 
